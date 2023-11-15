f261281a390288c26df6c00585e9d4f2ece31cea
Zach <132305891+PuzzleZach@users.noreply.github.com>
2023-11-14 18:06:55 -0600

    Create TestDriver.py

---
+++ TestDriver.py
@@ -0,0 +1,9 @@
+from Test.test_auth import test_auth
+from Test.test_builder import test_builder
+
+# Would use unittest in practice but this is faster.
+if __name__ == "__main__":
+  auth = test_auth()
+  build = test_builder()
+
+print(f"The result of our auth test is {auth} and the result for our build test is {build}")
