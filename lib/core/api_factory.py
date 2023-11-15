c8dd91aec96a54f1e2ec08d067010e73764aa8e3
Zach <132305891+PuzzleZach@users.noreply.github.com>
2023-11-14 18:00:03 -0600

    Create api_factory.py

---
+++ lib/core/api_factory.py
@@ -0,0 +1,17 @@
+from ..auth.auth_system import get_token
+
+class API_Endpoint:
+  def __init__(self, endpoint, header, body):
+    self.endpoint = endpoint
+    self.header = header
+    self.body = body
+
+class API_Builder:
+  def build(self, endpoint):
+    header = {"token":get_token()}
+    if endpoint == "Get-Health":
+      body = {"days_interval": 3}
+      return API_Endpoint(endpoint, header, body)
+
+def api_builder(endpoint):
+  return API_Builder(endpoint)
